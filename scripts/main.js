6c21b9ff7a359391c1fcbc743393115098373bf1
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2022-04-12 08:44:14 +0900

    Replace hello world page to practice-fe page

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+let myHeading = document.querySelector('h1')
+myHeading.textContent = 'Hello world!'
+
+
+let myHtml = document.querySelector('html')
+myHtml.ondblclick = function () {
+    alert('Ouch! stop poking me...')
+}
+
+let myImage = document.querySelector('img')
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src')
+    if ( mySrc === 'images/icon_google.png') {
+        myImage.setAttribute('src', 'images/icon_firefox.png')
+    } else {
+        myImage.setAttribute('src', 'images/icon_google.png')
+    }
+}
+
+let myButton = document.querySelector('button')
+let userName = "User"
+
+function setUserName() {
+    let myName = prompt('Tell me your name.')
+    localStorage.setItem('name', myName)
+    userName = myName
+
+}
+
+if ( !localStorage.getItem('name') ) {
+    setUserName()
+} else {
+    userName = localStorage.getItem('name')
+}
+
+myHeading.innerHTML = 'JS is cool, ' + userName
