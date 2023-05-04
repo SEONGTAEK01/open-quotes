032d10a690b61789f506e7d7bc8fa2f5bbc04a89
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2023-05-04 16:24:28 +0900

    ADD: Pilsu kim's quote

--- src/components/quotes.ts
+++ src/components/quotes.ts
@@ -5,6 +5,7 @@ const sentences = [
   "가구는 제작소에서 완성되는 것이 아니라 공간에서 완성된다.  - OREBRO Studio",
   "A gentleman is one who never hurts anyone's feelings unintentionally.  - Oscar Wilde",
   "If you no longer go for a gap that exists, you're no longer a racing driver.  - Ayrton Senna",
+  "솔직함이 최고의 무기이다.  - 필수 킴",
 ];
 
 export default sentences;
