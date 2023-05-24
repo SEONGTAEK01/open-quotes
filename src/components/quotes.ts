0bd0271f241638b208ff9f309de155155ef89b38
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2023-05-25 08:59:29 +0900

    ADD: 박준 산문집의 내용, FIX: animation ease secs

--- src/components/quotes.ts
+++ src/components/quotes.ts
@@ -8,6 +8,7 @@ const sentences = [
   "솔직함이 최고의 무기이다.  - 필수 킴",
   "참된 여행은 새로운 풍경을 찾는 게 아니라 새로운 눈을 갖는 것이다.  - Marcel Proust",
   "A leader is someone willing to make decisions.\nPoliticians can get elected if voters think they will do things, even if they don't sup-port all those things.\nW [President George W. Bush] was elected not because everyone agreed with him but because they knew he was sincere and would do what he thought needed to be done. - Michael Bloomberg (former New York mayor)",
+  "말은 사람의 입에서 태어났다가 사람의 귀에서 죽는다. 하지만 어떤 말들은 죽지 않고 사람의 마음속으로 들어가 살아남는다. - 박준 산문집 중",
 ];
 
 export default sentences;
